e5952c3126c430ead8914759c38f16a286cdb80f
nja-006 <lazarus.jason09@gmail.com>
2020-08-06 21:20:39 +0700

    migration

---
+++ database/migrations/2020_08_06_135619_create_komentar_pertanyaans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKomentarPertanyaansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('komentar_pertanyaans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('isi');
+            $table->date('tanggal_dibuat');
+            $table->unsignedBigInteger('pertanyaan_id');
+            $table->unsignedBigInteger('profil_id');
+
+            $table->foreign('profil_id')->references('id')->on('users');
+            $table->foreign('pertanyaan_id')->references('id')->on('pertanyaans');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('komentar_pertanyaans');
+    }
+}
